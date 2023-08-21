9617978ccdf5292819913eef03824f690583a830
Naresh081 <nareshm@b2btesters.com>
2023-08-21 06:57:36 +0000

    Good Reads Books

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const { open } = require("sqlite");
+const path = require("path");
+const sqlite3 = require("sqlite3");
+
+const dbPath = path.join(__dirname, "goodreads.db");
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
+
+app.get("/books/", async (request, response) => {
+  const getBooksQuery = `
+    
+    SELECT * FROM book ORDER BY book_id;
+    `;
+
+  const bookArray = await db.all(getBooksQuery);
+
+  response.send(bookArray);
+});
